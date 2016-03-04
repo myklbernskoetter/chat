acec2d7960c52f1abafe05a17e08fd475de271d5
Mykl Bernskoetter <mykl@40digits.com>
2016-03-04 13:56:37 -0600

    Cleaned up some JS spacing and indentation in the main.js file

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,13 +3,12 @@
 (function(){
 
   var socket = io();
+
   var name = prompt('What is your name?');
-
-
   $('#name').val(name);
 
   //Putting in some indicators that the chat is either active or inactive.
-  $(".disconnect").on('click tap touch', function() {
+  $(".disconnect").on('click tap touch', function () {
     socket.close();
     $(this).css('display', 'none');
     $('.connect').css('display', 'inline');
@@ -19,7 +18,7 @@
 
   });
 
-  $(".connect").on('click tap touch', function() {
+  $(".connect").on('click tap touch', function () {
     socket.connect();
     $(this).css('display', 'none');
     $('.disconnect').css('display', 'inline');
@@ -27,18 +26,17 @@
     $('#output').css('outline', 'none');
   });
 
-  // The User gets to choos their own color and it shows up in several places- the header, the chat banner section, and their messages.
-  $('#color').bind('input', function() {
+  // The User gets to choose their own color and it shows up in several places- the header, the chat banner section, and their messages.
+  $('#color').bind('input', function () {
     var color = $(this).val(); // get the current value of the input field.
-    // $('h1').css('color', color);
     $('header').css('background', color);
   });
 
   // We Only need to run the function if we're focused
   // on the chat-input and push enter to submit it.
   if( $("#chat-input").on(':focus')) {
-    $(document).keypress(function(e) {
-      if( e.which == 13 ) {
+    $(document).keypress(function (e) {
+      if( e.which === 13 ) {
         e.preventDefault();
         submitMessage();
       }
@@ -46,12 +44,12 @@
   }
 
   //  Currently, clicking an emoji in the emoji container will  add emojis to the input line as they're writing their messages
-  $(".emoji").on('click tap touch', function(e) {
+  $(".emoji").on('click tap touch', function (e) {
     var emoji = $(this).attr('src');
     $('#chat-input').append('<img class="appendedEmoji" src="' + emoji + '"/>');
   });
 
-// Output 1 of 2 ways.  If connnected- run the submitMessage function which then emits
+  // Output 1 of 2 ways.  If connnected- run the submitMessage function which then emits through the socketio server.  Otherwise it just posts to the local user's window.
   function submitMessage() {
     var name = $('#name').val(),
     message = $('#chat-input').html(),
@@ -61,8 +59,8 @@
     msg = '',
     color = $('#color').val(),
     time = timeStamp();
-    if( name != '' && message != '' ) {
-      if(socket.connected === true) {
+    if (name && message) {
+      if (socket.connected) {
         msg = '<div class="speech"><p class="output" style="border: 2px solid ' + color + '" > <span class="name ' + name +'" >[' + time + '] ' +
         name + ':</span> ' + parsedMessage + '</p></div>';
 
@@ -70,13 +68,12 @@
       } else {
         msg = '<div class="talk-bubble tri-right left-in"><p class="output not-sent"> <span class="not-sent" style="color: ' + color + '">[' + time + '] ' +
         name + ':</span> ' + parsedMessage + '</p></div>';
-
         output(msg);
       }
     }
   }
 
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function (msg) {
     output(msg);
   });
 
